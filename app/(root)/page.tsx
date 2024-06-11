5c81c003217abb6d2a51dd875b47e2093c1a977b
Gurvir Singh <gurvirsingh.baraich@gmail.com>
2024-06-11 11:59:45 +0530

    make landing page responsive

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -53,12 +53,12 @@ export default async function LandingPage() {
 
       <div className="overflow-y-auto overflow-hidden h-full">
         <div className="grid grid-cols-1 gird-rows-1 h-auto min-h-screen">
-          <div className="py-48 bg-slate-900">
-            <div className="w-max mx-auto flex space-y-8 flex-col">
-              <h1 className="text-white text-7xl font-bold">
+          <div className="lg:py-48 py-24 bg-slate-900">
+            <div className="w-max mx-auto flex space-y-8 flex-col lg:w-auto max-w-[80%]">
+              <h1 className="text-white text-2xl text-balance lg:text-7xl font-bold">
                 Effortlessly Convert Raw Data to Dynamic JSON.
               </h1>
-              <p className="text-2xl text-stone-200">
+              <p className="text-base lg:text-2xl text-stone-200">
                 Never worry about processing data again with our reliable and
                 scalable API for developers.
               </p>
@@ -69,7 +69,7 @@ export default async function LandingPage() {
             <FaArrowDownLong className="text-white" size={24} />
           </div>
 
-          <div className="grid w-full gap-4 grid-cols-[1fr_50px_1fr_50px_1fr] h-max grid-rows-1 px-[5.6rem]">
+          <div className="grid w-full place-items-center gap-4 xl:grid-cols-[1fr_50px_1fr_50px_1fr] h-max xl:grid-rows-1 grid-cols-1 max-w-[80%] xl:max-w-a xl:max-w-max mx-auto md:px-[5.6rem]">
             <div className="w-full">
               <div className="p-4 border-b bg-stone-100 text-center border-stone-400">
                 Input Raw Data
@@ -78,7 +78,7 @@ export default async function LandingPage() {
                 <div className="border-r border-stone-400 p-3 py-4">
                   <span className="bg-orange-400 text-white p-2">POST</span>
                 </div>
-                <div className="p-4">
+                <div className="p-4 truncate">
                   <span className="underline">
                     https://api.data2json.xyz/v1
                   </span>
@@ -93,7 +93,7 @@ export default async function LandingPage() {
               </div>
             </div>
 
-            <div className="w-full h-full flex items-center justify-center">
+            <div className="w-full h-full rotate-90 xl:rotate-0 flex items-center justify-center">
               <FaArrowRightLong className="text-white" size={32} />
             </div>
 
@@ -105,7 +105,7 @@ export default async function LandingPage() {
                 <div className="border-r border-stone-400 p-3 py-4">
                   <span className="bg-orange-400 text-white p-2">POST</span>
                 </div>
-                <div className="p-4">
+                <div className="p-4 truncate">
                   <span className="underline">
                     https://api.data2json.xyz/v1
                   </span>
@@ -120,11 +120,11 @@ export default async function LandingPage() {
               </div>
             </div>
 
-            <div className="w-full h-full flex items-center justify-center">
+            <div className="w-full rotate-90 xl:rotate-0 h-full flex items-center justify-center">
               <FaArrowRightLong className="text-white" size={32} />
             </div>
 
-            <div className="max-w-lg w-full">
+            <div className="w-full">
               <div className="p-4 border-b text-center border-stone-400 bg-stone-100">
                 API Response
               </div>
@@ -132,7 +132,7 @@ export default async function LandingPage() {
                 <div className="border-r border-stone-400 p-3 py-4">
                   <span className="bg-orange-400 text-white p-2">POST</span>
                 </div>
-                <div className="p-4">
+                <div className="p-4 truncate">
                   <span className="underline">
                     https://api.data2json.xyz/v1
                   </span>
